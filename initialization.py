37b587a42996ca48dd4cddc0815f0fcfb71ad87d
ma0274ni <ma0274ni@ma0274ni-wifi.nano.lu.se>
2023-04-26 13:59:03 +0200

    Finished implementation of hamiltonian, Included eigenvalue investigations with sweeps over zeeman and chemical potential

--- initialization.py
+++ initialization.py
@@ -1,6 +1,7 @@
 # import the nanowire_hamiltonian package
 import importlib
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 import nanowire_hamiltonian_class as nh
@@ -12,6 +13,9 @@ import potential_barrier_class as pb
 importlib.reload(pb)
 importlib.reload(nh)
 
+# reduce the number of decimals for printing in numpy
+np.set_printoptions(precision=8)
+
 
 def potential_function(x, x0, sigma):
     return np.exp((x - x0)**2 / (2 * sigma**2))
@@ -21,7 +25,7 @@ def initialize_standard_hamiltonian():
     # define the potential barrier values
     x0 = 0.0
     barrier_height = 10.0
-    sigma = 10.0
+    sigma = 100.0
 
     # define the hamiltonian parameters, energies in meV, length in m, mass in m_e
     alpha = 50.0
@@ -30,16 +34,87 @@ def initialize_standard_hamiltonian():
     sc_gap = 0.5
     eff_mass = 0.015
     nw_length = 4e+3
-    grid_points = 100
+    position_grid = 100
 
-    # initialize the potential barrier
+    # initialize the potential barriek
     poti = pb.potential_barrier(potential_function, x0, sigma, barrier_height)
 
-    # initialize the hamiltonian
+    # initialize, build and diagonalize the hamiltonian
     hami = nh.nanowire_hamiltonian(alpha, zeeman, chem_pot, sc_gap, eff_mass,
-                                   nw_length, grid_points, poti)
-    hami.greet()
-    hami.build_hamiltonian(0.0)
+                                   nw_length, position_grid, poti)
+
+    # Do a sweep over zeeman field, initialize the hamiltonian and diagonalize for each value
+    # do_zeeman_sweep(alpha, chem_pot, sc_gap, eff_mass, nw_length,
+    #                 position_grid, poti)
+
+    # Only compute two eigenvalues closest to zero
+    # use argsort to sort the eigenvalues for their absolute value
+    hami = nh.nanowire_hamiltonian(alpha, zeeman, chem_pot, sc_gap, eff_mass,
+                                   nw_length, position_grid, poti)
+    hamiltonian_matrix = hami.build_hamiltonian()
+    eigvals = np.linalg.eigvalsh(hamiltonian_matrix)
+    ordering = np.abs(eigvals).argsort()
+    eigvals = eigvals[ordering]
+
+    print("The two lowest eigenvalues are:", eigvals[0], "and", eigvals[1])
+    delta_e = eigvals[1] - eigvals[0]
+    print("The energy difference between the two lowest eigenvalues is:",
+          delta_e)
+
+    # Perform a 2D sweep over the chemical potential and the Zeeman field
+    # initialize the hamiltonian and diagonalize for each value
+    # use argsort to sort the eigenvalues for their absolute value
+    # plot the absolute difference between the two lowest eigenvalues as a function of the chemical potential and the Zeeman field
+    # perform calculation and sweep in an external function called do_2d_sweep
+    do_2d_sweep(alpha, chem_pot, sc_gap, eff_mass, nw_length, position_grid,
+                poti)
+
+
+def do_2d_sweep(alpha, chem_pot, sc_gap, eff_mass, nw_length, position_grid,
+                poti):
+    zeeman_sweep = np.linspace(0.0, 20.0, 10)
+    chem_pot_sweep = np.linspace(0.0, 20.0, 10)
+    delta_e_sweep = []
+    for i, zeeman in enumerate(zeeman_sweep):
+        delta_e_sweep.append([])
+        for j, chem_pot in enumerate(chem_pot_sweep):
+            hami = nh.nanowire_hamiltonian(alpha, zeeman, chem_pot, sc_gap,
+                                           eff_mass, nw_length, position_grid,
+                                           poti)
+            hamiltonian_matrix = hami.build_hamiltonian()
+            eigenvalues, eigenvectors = np.linalg.eigh(hamiltonian_matrix)
+            ordering = np.abs(eigenvalues).argsort()
+            eigenvalues = eigenvalues[ordering]
+            delta_e_sweep[i].append(np.abs(eigenvalues[1] - eigenvalues[0]))
+
+    # Create a 2D plot of the energy difference as a function of the chemical potential and the Zeeman field
+    delta_e_sweep = np.array(delta_e_sweep)
+    plt.imshow(delta_e_sweep, extent=[0.0, 20.0, 0.0, 20.0])
+    plt.xlabel('Chemical potential (meV)')
+    plt.ylabel('Zeeman field (meV)')
+    plt.colorbar()
+    plt.show()
+
+
+def do_zeeman_sweep(alpha, chem_pot, sc_gap, eff_mass, nw_length,
+                    position_grid, poti):
+    zeeman_sweep = np.linspace(0.0, 20.0, 10)
+    eigenvalues_sweep = []
+    for i, zeeman in enumerate(zeeman_sweep):
+        hami = nh.nanowire_hamiltonian(alpha, zeeman, chem_pot, sc_gap,
+                                       eff_mass, nw_length, position_grid,
+                                       poti)
+        hamiltonian_matrix = hami.build_hamiltonian()
+        eigenvalues, eigenvectors = np.linalg.eigh(hamiltonian_matrix)
+        eigenvalues_sweep.append(eigenvalues)
+
+    # Plot the eigenvalues as a function of zeeman field
+    eigenvalues_sweep = np.array(eigenvalues_sweep)
+    plt.plot(zeeman_sweep, eigenvalues_sweep)
+    plt.xlabel('Zeeman field (meV)')
+    plt.ylabel('Eigenvalues (meV)')
+    plt.ylim(-100.0, 100.0)
+    plt.show()
 
 
 def main():
--- nanowire_hamiltonian_class.py
+++ nanowire_hamiltonian_class.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.linalg import block_diag
 
 import unit_conversion as uc
 
@@ -6,7 +7,7 @@ import unit_conversion as uc
 
 
 class nanowire_hamiltonian:
-    # Initialize the Hamiltonian
+    # Initialize the Hamiltonian, pot_func is a class that represents the potential
     def __init__(self, alpha, zeeman, chem_pot, sc_gap, eff_mass, nw_length,
                  grid_points, pot_func):
         self.alpha = alpha
@@ -46,7 +47,7 @@ class nanowire_hamiltonian:
             "Hello! I hope you have a nice day! I am a nanowire Hamiltonian.")
 
     # Function that builds the matrix representation of the Hamiltonian
-    def build_hamiltonian(self, x):
+    def build_hamiltonian(self):
         # Define the sigma matrices using numpy
         sigma_0 = np.array([[1, 0], [0, 1]])
         sigma_x = np.array([[0, 1], [1, 0]])
@@ -59,8 +60,33 @@ class nanowire_hamiltonian:
         block3 = np.kron(sigma_x, sigma_0)
         block4 = np.kron(sigma_0, sigma_x)
 
-        # Build the terms using the previously defined blocks
-        diag_block = (-2 / self.dx**2 * 1 / (2 * self.eff_mass) -
-                      self.chem_pot + self.evaluate_potential(x)
-                      ) * block1 + self.sc_gap * block3 + self.zeeman * block4
-        print("Diagonal block 1: \n", diag_block)
+        # Build the diagonal terms
+        diag_block_const = (
+            -2 / self.dx**2 * 1 / (2 * self.eff_mass) - self.chem_pot
+        ) * block1 + self.sc_gap * block3 + self.zeeman * block4
+        diag_block = np.array([
+            diag_block_const + self.evaluate_potential(site) * block1
+            for site in self.x
+        ])
+        # Cast diag_block to complex
+        diag_block = diag_block.astype(complex)
+
+        # Build the off-diagonal terms
+        upper_off_diag_block = +1 / self.dx**2 * 1 / (
+            2 * self.eff_mass) * block1 - 1j * self.alpha * (+1) * block2
+        lower_off_diag_block = +1 / self.dx**2 * 1 / (
+            2 * self.eff_mass) * block1 - 1j * self.alpha * (-1) * block2
+        upper_off_diag = np.array(
+            [upper_off_diag_block for site in self.x[1:]])
+        lower_off_diag = np.array(
+            [lower_off_diag_block for site in self.x[:-1]])
+
+        # Next we build hamiltonian such that the diagonal conists of diag_block and the off-diagonal consists of upper_off_diag and lower_off_diag
+        offset = 4
+        aux = np.empty((0, offset), int)
+
+        hamiltonian = block_diag(*diag_block)
+        hamiltonian += block_diag(aux, *upper_off_diag, aux.T)
+        hamiltonian += block_diag(aux.T, *lower_off_diag, aux)
+
+        return hamiltonian / self.sc_gap
