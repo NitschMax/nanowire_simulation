98b0ed00a97cf2074f7002c72e97a2710b3e233a
ma0274ni <ma0274ni@ma0274ni-wifi.nano.lu.se>
2023-04-28 14:27:33 +0200

    Added routines for zeeman sweeps and wavefunction plots in initialization, routines for diagonalization in nh_class

--- initialization.py
+++ initialization.py
@@ -18,23 +18,23 @@ np.set_printoptions(precision=8)
 
 
 def potential_function(x, x0, sigma):
-    return np.exp((x - x0)**2 / (2 * sigma**2))
+    return np.exp(-(x - x0)**2 / (2 * sigma**2))
 
 
 def initialize_standard_hamiltonian():
     # define the potential barrier values
     x0 = 0.0
-    barrier_height = 10.0
-    sigma = 100.0
+    barrier_height = 5.0
+    sigma = 10.0
 
     # define the hamiltonian parameters, energies in meV, length in m, mass in m_e
-    alpha = 50.0
+    alpha = 5.0
     zeeman = 2.0
-    chem_pot = 5.0
-    sc_gap = 0.5
+    chem_pot = 2.0
+    sc_gap = 5.0
     eff_mass = 0.015
-    nw_length = 4e+3
-    position_grid = 100
+    nw_length = 2e+2
+    position_grid = 400
 
     # initialize the potential barriek
     poti = pb.potential_barrier(potential_function, x0, sigma, barrier_height)
@@ -43,77 +43,134 @@ def initialize_standard_hamiltonian():
     hami = nh.nanowire_hamiltonian(alpha, zeeman, chem_pot, sc_gap, eff_mass,
                                    nw_length, position_grid, poti)
 
-    # Do a sweep over zeeman field, initialize the hamiltonian and diagonalize for each value
-    # do_zeeman_sweep(alpha, chem_pot, sc_gap, eff_mass, nw_length,
-    #                 position_grid, poti)
+    hami.build_hamiltonian()
+    # Compare diagonalization methods for hami
+    # hami.compare_diagonalization_methods()
 
-    # Only compute two eigenvalues closest to zero
-    # use argsort to sort the eigenvalues for their absolute value
+    # Introduce flags to control zeeman_sweep and phase_sweep
+    zeeman_sweep_flag = True
+    plot_wavefunctions_flag = True
+    phase_sweep_flag = False
+
+    if zeeman_sweep_flag:
+        zeeman_sweep(alpha,
+                     chem_pot,
+                     sc_gap,
+                     eff_mass,
+                     nw_length,
+                     position_grid,
+                     poti,
+                     num_eigenvalues=100)
+
+    if plot_wavefunctions_flag:
+        zeeman = 10.0
+        plot_wavefunctions(alpha, zeeman, chem_pot, sc_gap, eff_mass,
+                           nw_length, position_grid, poti)
+
+    if phase_sweep_flag:
+        phase_sweep(alpha, chem_pot, sc_gap, eff_mass, nw_length,
+                    position_grid, poti)
+
+
+# Define a function that calculates and plots the absolute value of the wavefunctions of the two lowest eigenvalues
+
+
+def plot_wavefunctions(alpha, zeeman, chem_pot, sc_gap, eff_mass, nw_length,
+                       position_grid, poti):
     hami = nh.nanowire_hamiltonian(alpha, zeeman, chem_pot, sc_gap, eff_mass,
                                    nw_length, position_grid, poti)
-    hamiltonian_matrix = hami.build_hamiltonian()
-    eigvals = np.linalg.eigvalsh(hamiltonian_matrix)
-    ordering = np.abs(eigvals).argsort()
-    eigvals = eigvals[ordering]
+    hami.build_hamiltonian()
+    hami.diagonalize_hamiltonian()
+    eigenvalues, eigenvectors = hami.get_smallest_eigenvalues_and_vectors(2)
+    abs_eigenvectors = hami.calculate_abs_wavefunctions(eigenvectors)
+    abs_eigenvectors = hami.calculate_abs_gamma_wavefunctions(eigenvectors)
 
-    print("The two lowest eigenvalues are:", eigvals[0], "and", eigvals[1])
-    delta_e = eigvals[1] - eigvals[0]
-    print("The energy difference between the two lowest eigenvalues is:",
-          delta_e)
+    print('The two lowest eigenvalues are: ', eigenvalues)
+    # Include second axis for the potential barrier
 
-    # Perform a 2D sweep over the chemical potential and the Zeeman field
-    # initialize the hamiltonian and diagonalize for each value
-    # use argsort to sort the eigenvalues for their absolute value
-    # plot the absolute difference between the two lowest eigenvalues as a function of the chemical potential and the Zeeman field
-    # perform calculation and sweep in an external function called do_2d_sweep
-    do_2d_sweep(alpha, chem_pot, sc_gap, eff_mass, nw_length, position_grid,
-                poti)
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+    ax1.plot(hami.x, abs_eigenvectors[0], 'r')
+    ax1.plot(hami.x, abs_eigenvectors[1], 'g')
+    ax2.plot(hami.x, poti(hami.x) / hami.sc_gap, 'b')
+    ax1.set_xlabel('Position (nm)')
+    ax1.set_ylabel('Wavefunction')
+    # Potential barrier is scaled by the superconducting gap
+    ax2.set_ylabel(r'Potential barrier ($\Delta$)')
+    plt.show()
 
 
-def do_2d_sweep(alpha, chem_pot, sc_gap, eff_mass, nw_length, position_grid,
+def phase_sweep(alpha, chem_pot, sc_gap, eff_mass, nw_length, position_grid,
                 poti):
-    zeeman_sweep = np.linspace(0.0, 20.0, 10)
-    chem_pot_sweep = np.linspace(0.0, 20.0, 10)
+    chem_min = -3.0
+    chem_max = 8.0
+    zeeman_min = 0.0
+    zeeman_max = 8.0
+    grid_size = 16
+    chem_pot_sweep = np.linspace(chem_min, chem_max, grid_size)
+    zeeman_sweep = np.linspace(zeeman_min, zeeman_max, grid_size)
     delta_e_sweep = []
-    for i, zeeman in enumerate(zeeman_sweep):
+    for i, chem_pot in enumerate(chem_pot_sweep):
         delta_e_sweep.append([])
-        for j, chem_pot in enumerate(chem_pot_sweep):
+        for j, zeeman in enumerate(zeeman_sweep):
+            # print message to keep track of the calculation
+            print('Calculating for chem_pot = ', chem_pot, ' and zeeman = ',
+                  zeeman)
+            print('Progress: ', i * grid_size + j + 1, '/', grid_size**2)
             hami = nh.nanowire_hamiltonian(alpha, zeeman, chem_pot, sc_gap,
                                            eff_mass, nw_length, position_grid,
                                            poti)
-            hamiltonian_matrix = hami.build_hamiltonian()
-            eigenvalues, eigenvectors = np.linalg.eigh(hamiltonian_matrix)
-            ordering = np.abs(eigenvalues).argsort()
-            eigenvalues = eigenvalues[ordering]
+            hami.build_hamiltonian()
+            hami.diagonalize_hamiltonian()
+            eigenvalues, eigenvectors = hami.get_smallest_eigenvalues_and_vectors(
+                2)
             delta_e_sweep[i].append(np.abs(eigenvalues[1] - eigenvalues[0]))
 
     # Create a 2D plot of the energy difference as a function of the chemical potential and the Zeeman field
+    # show colorbar in log scale
     delta_e_sweep = np.array(delta_e_sweep)
-    plt.imshow(delta_e_sweep, extent=[0.0, 20.0, 0.0, 20.0])
-    plt.xlabel('Chemical potential (meV)')
-    plt.ylabel('Zeeman field (meV)')
+    delta_e_sweep = np.log10(delta_e_sweep)
+    plt.imshow(delta_e_sweep,
+               extent=[zeeman_min, zeeman_max, chem_min, chem_max])
+
+    plt.xlabel('Zeeman field (meV)')
+    plt.ylabel('Chemical potential (meV)')
     plt.colorbar()
     plt.show()
 
 
-def do_zeeman_sweep(alpha, chem_pot, sc_gap, eff_mass, nw_length,
-                    position_grid, poti):
-    zeeman_sweep = np.linspace(0.0, 20.0, 10)
+def zeeman_sweep(alpha,
+                 chem_pot,
+                 sc_gap,
+                 eff_mass,
+                 nw_length,
+                 position_grid,
+                 poti,
+                 num_eigenvalues=10):
+    zeeman_sweep = np.linspace(0.0, 10.0, 20)
     eigenvalues_sweep = []
     for i, zeeman in enumerate(zeeman_sweep):
         hami = nh.nanowire_hamiltonian(alpha, zeeman, chem_pot, sc_gap,
                                        eff_mass, nw_length, position_grid,
                                        poti)
-        hamiltonian_matrix = hami.build_hamiltonian()
-        eigenvalues, eigenvectors = np.linalg.eigh(hamiltonian_matrix)
+        print('Zeeman field: ', zeeman)
+        print('Run', i + 1, 'of', len(zeeman_sweep))
+        hami.build_hamiltonian()
+        hami.diagonalize_hamiltonian()
+        eigenvalues, eigenvectors = hami.get_smallest_eigenvalues_and_vectors(
+            num_eigenvalues)
         eigenvalues_sweep.append(eigenvalues)
 
     # Plot the eigenvalues as a function of zeeman field
     eigenvalues_sweep = np.array(eigenvalues_sweep)
-    plt.plot(zeeman_sweep, eigenvalues_sweep)
+    plt.plot(zeeman_sweep, eigenvalues_sweep, 'k')
     plt.xlabel('Zeeman field (meV)')
-    plt.ylabel('Eigenvalues (meV)')
-    plt.ylim(-100.0, 100.0)
+    # ylabel energy in units of sc_gap
+    plt.ylabel(r'$E [\Delta]$')
+
+    plt.yticks(np.arange(-10, 10, 1.0))
+    plt.ylim(-2, 2)
+
     plt.show()
 
 
--- nanowire_hamiltonian_class.py
+++ nanowire_hamiltonian_class.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.linalg import block_diag
+from scipy.sparse.linalg import eigsh
 
 import unit_conversion as uc
 
@@ -32,6 +33,7 @@ class nanowire_hamiltonian:
         self.chem_pot = self.chem_pot * energy_conversion
         self.sc_gap = self.sc_gap * energy_conversion
         self.eff_mass = self.eff_mass * mass_conversion
+        self.nw_length = self.nw_length * length_conversion
 
     # Initialize the grid
     def initialize_grid(self):
@@ -62,7 +64,7 @@ class nanowire_hamiltonian:
 
         # Build the diagonal terms
         diag_block_const = (
-            -2 / self.dx**2 * 1 / (2 * self.eff_mass) - self.chem_pot
+            +2 / self.dx**2 * 1 / (2 * self.eff_mass) - self.chem_pot
         ) * block1 + self.sc_gap * block3 + self.zeeman * block4
         diag_block = np.array([
             diag_block_const + self.evaluate_potential(site) * block1
@@ -72,9 +74,9 @@ class nanowire_hamiltonian:
         diag_block = diag_block.astype(complex)
 
         # Build the off-diagonal terms
-        upper_off_diag_block = +1 / self.dx**2 * 1 / (
+        upper_off_diag_block = -1 / self.dx**2 * 1 / (
             2 * self.eff_mass) * block1 - 1j * self.alpha * (+1) * block2
-        lower_off_diag_block = +1 / self.dx**2 * 1 / (
+        lower_off_diag_block = -1 / self.dx**2 * 1 / (
             2 * self.eff_mass) * block1 - 1j * self.alpha * (-1) * block2
         upper_off_diag = np.array(
             [upper_off_diag_block for site in self.x[1:]])
@@ -89,4 +91,53 @@ class nanowire_hamiltonian:
         hamiltonian += block_diag(aux, *upper_off_diag, aux.T)
         hamiltonian += block_diag(aux.T, *lower_off_diag, aux)
 
+        self.hamiltonian = hamiltonian
+
         return hamiltonian / self.sc_gap
+
+    def diagonalize_hamiltonian(self):
+        self.eigvals, self.eigvecs = np.linalg.eigh(self.hamiltonian)
+        return self.eigvals / self.sc_gap, self.eigvecs
+
+    def get_smallest_eigenvalues_and_vectors(self, num_eigvals):
+        order = np.argsort(np.abs(self.eigvals))
+        result_eigvals = self.eigvals[order][:num_eigvals]
+        result_eigvecs = self.eigvecs[:, order][:, :num_eigvals]
+
+        re_order = np.argsort(result_eigvals)
+        result_eigvals = result_eigvals[re_order]
+        result_eigvecs = result_eigvecs[:, re_order]
+
+        return result_eigvals / self.sc_gap, result_eigvecs
+
+    # Function to calculate the absolute value of the wavefunction on each site
+    # This corresponds to the sum of the electron and hole and spin up and spin down wavefunctions on each site
+    def calculate_abs_wavefunctions(self, eigvecs):
+        abs_wavefunction = np.abs(eigvecs[0::4])**2 + np.abs(
+            eigvecs[1::4])**2 + np.abs(eigvecs[2::4])**2 + np.abs(
+                eigvecs[3::4])**2
+        return abs_wavefunction
+
+    # Calculate the absolute value of the psi_0 + i psi_1 wavefunction on each site and
+    def calculate_abs_gamma_wavefunctions(self, eigvecs):
+        psi_plus = eigvecs[:, 0] + eigvecs[:, 1]
+        abs_psi_plus = self.calculate_abs_wavefunctions(psi_plus)
+
+        psi_minus = eigvecs[:, 0] - eigvecs[:, 1]
+        abs_psi_minus = self.calculate_abs_wavefunctions(psi_minus)
+
+        return abs_psi_plus, abs_psi_minus
+
+    # Routine that only calculates the eigenvalues with the smallest absolute value via eigsh
+    def calculate_only_smallest_eigenvalues(self, num_eigvals=10):
+        hamiltonian = self.build_hamiltonian()
+        eigvals, eigvecs = eigsh(hamiltonian, k=num_eigvals, which='SM')
+        return eigvals / self.sc_gap, eigvecs
+
+    # Routine that compares differen diagonalization methods
+    def compare_diagonalization_methods(self):
+        self.build_hamiltonian()
+
+        print(self.diagonalize_hamiltonian()[0])
+        print(self.get_smallest_eigenvalues_and_vectors(4)[0])
+        print(self.calculate_only_smallest_eigenvalues(10)[0])
